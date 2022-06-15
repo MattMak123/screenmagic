34f28f79ba5e9affc682a3e23675a0deb59be0cd
MattMak123 <91562671+MattMak123@users.noreply.github.com>
2022-06-15 15:04:33 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 let i = 0
-for(let i=0; i<25; ++i){
+for(let i=0; i<25; i++){
     screenMagic.plottingLED(i)
     basic.pause(100)
     i++
