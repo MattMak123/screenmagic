a619678f6e3a9e26ad5f2d62708811e94d767e98
MattMak123 <91562671+MattMak123@users.noreply.github.com>
2022-06-15 15:00:43 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ namespace screenMagic {
     /**
      * address LEDs linearly row first
      */
-    //%blockId=screenmagicplotat
+    //% blockId=screenmagicplotat
+    //% block="plot at $index"
+    //% index.min=0 index.max=1000
     export function plottingLED(index: number): void {
         index |= 0
         const x = Math.floor(index % 5)
