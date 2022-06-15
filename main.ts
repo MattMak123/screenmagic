29082b7c5363ae0cb7b62b08cff174136bfef349
MattMak123 <91562671+MattMak123@users.noreply.github.com>
2022-06-15 14:46:46 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic {
+    /**
+     * address LEDs linearly row first
+     */
+    //%blockId=screenmagicplotat
+    export function lineIndex(index: number): void {
+        index |= 0
+        const x = Math.floor(index % 5)
+        const y = Math.floor(index / 5)
+        led.plot(x, y)
+    }
+}
